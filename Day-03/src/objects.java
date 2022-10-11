0690aa5d3d8def38145ad1b34ef2e5e89468ab60
BETHEGNT <tyagigagan30@gmail.com>
2022-10-11 17:22:17 +0530

    update

--- Day-03/src/objects.java
+++ Day-03/src/objects.java
@@ -2,18 +2,33 @@ import java.util.Scanner;
  class Animal{
         int legs;
         String name;
+         void walk(){
+             System.out.println(name +" "+ "is walking");
+         }
+         void walk(int steps){
+             System.out.println("Animal walked " +steps+" steps ");
+         } // method overloading.
+         public Animal(){
+             System.out.println("calling the constructor");
+         }
+
+         public Animal(String newName){
+             System.out.println(newName+"was passed while creating ");
+         }
     }
 
 public class objects{
     public static void main(String[]args) {
      Animal a1 = new Animal();
-     Animal a2 = new Animal();   
+     Animal a2 = new Animal("bob");
 
      a1.name = "Simba";
      a1.legs = 4;
 
      a2.name ="BOB";
      a2.legs = 2;
+     a1.walk();
+     a1.walk(5);
      System.out.println(a1.name + " "+ a1.legs);
     }
    
