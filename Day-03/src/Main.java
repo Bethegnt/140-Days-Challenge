b03640c4537b42e4bc189ca1c4c5fdf48385b044
BETHEGNT <tyagigagan30@gmail.com>
2022-10-12 01:05:06 +0530

    update

--- Day-03/src/Main.java
+++ Day-03/src/Main.java
@@ -1,5 +1,32 @@
+
+class Person{
+    String name;
+    int age;
+    static int count;
+    public  Person(){
+        count++;
+        System.out.println("person is being created");
+    }
+    //    Person(String newName, int newAge){
+//        name = newName;
+//        age = newAge;
+//    } this is old factor which is used for the new name
+    Person(String name, int Age){
+        this();
+        this.name = name;
+        this.age = age;
+        walking(this);
+    }
+    void walking(Person obj){
+        System.out.println(name+ "is walking");
+        age +=10;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person p1 = new Person("Gagan",25);
+        System.out.println(p1.name+ " "+p1.age);
+        System.out.println(Person.count);
     }
 }
--- Day-04/src/Main.java
+++ Day-04/src/Main.java
@@ -1,5 +1,29 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Alien obj = new Alien();
+//      obj.legs = 3;
+        obj.speak();
+
     }
 }
+      class Alien{
+    static boolean isMemberOfEarth = false;
+    int legs;
+    void speak(){
+        System.out.println("Alien speak");
+    }
+    void sayHello(){
+          System.out.println("alien says hello");
+      }
+
+}
+
+
+
+
+
+
+
+
