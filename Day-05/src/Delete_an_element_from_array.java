e58fd4193f5c0ee9cbf9cba26735d3f01aa04c0c
BETHEGNT <tyagigagan30@gmail.com>
2022-10-13 23:33:49 +0530

    update

--- Day-05/src/Delete_an_element_from_array.java
+++ Day-05/src/Delete_an_element_from_array.java
@@ -10,17 +10,18 @@ public class Delete_an_element_from_array {
 
         }
         if (i == n) return n;
-            for (int j = 0; j < n-1; j++) {
-                a[j] = a[j+1];
+        for (int j = 0; j < n - 1; j++) {
+            a[j] = a[j + 1];
 
-            }
-            return n-1;
         }
-        public static void main(String[]args){
-            int a[] = {2, 1, 4, 6, 5};
-            int key = 1;
-            delete(a, key);
-            for(int e: a)
-                System.out.println(e+" ");
-        }
+        return n - 1;
     }
+
+    public static void main(String[] args) {
+        int a[] = {2, 1, 4, 6, 5};
+        int key = 1;
+        delete(a, key);
+        for (int e : a)
+            System.out.println(e + " ");
+    }
+}
---
+++ Day-05/src/Largest_Element_in_array.java
@@ -0,0 +1,9 @@
+public class Largest_Element_in_array {
+    public static void main(String[] args) {
+        int a[] = {4,1,7,2,3};
+        int max = 0;
+        for (int i = 0; i <n; i++) {
+
+        }
+    }
+}
