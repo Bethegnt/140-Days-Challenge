932d84b220347bd6a91da650d7751bf4990d978a
BETHEGNT <tyagigagan30@gmail.com>
2022-10-21 17:04:40 +0530

    update

---
+++ Day-05/src/Arraylist.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<>(5);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+        list.add(23);
+        list.add(12);
+
+        System.out.println(list.contains(23));
+        System.out.println(list);
+
+
+
+
+
+    }
+}
