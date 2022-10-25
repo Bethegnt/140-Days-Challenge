f8a587edaa37885fae35e50c973f8cf6722af9f1
BETHEGNT <tyagigagan30@gmail.com>
2022-10-25 19:00:08 +0530

    update

---
+++ Day-01/src/Datatypes.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        {
+            System.out.println("Int min: "+Integer.MIN_VALUE);
+            System.out.println("Int Max: "+Integer.MAX_VALUE);
+            System.out.println("Int Bytes: "+Integer.BYTES);
+
+        }
+    }
+
+}
