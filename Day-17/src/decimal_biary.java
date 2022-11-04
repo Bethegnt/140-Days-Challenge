8e20cb64f65c7a32defe9da1ded08a588a9e0c48
BETHEGNT <tyagigagan30@gmail.com>
2022-11-04 13:14:07 +0530

    update

--- Day-17/src/decimal_biary.java
+++ Day-17/src/decimal_biary.java
@@ -1,3 +1,21 @@
 public class decimal_biary {
+    public static void dectobin(int n){
+//        int remainder;
+        int myNum = n;
+        int Pow=0;
+        int binnum = 0;
+        while(n>0){
+            int remaider =n%2;
+            binnum = binnum +(remaider*(int)Math.pow(10 ,Pow));
+            Pow++;
+            n = n/2; // Quotient nikal rhe hai yahn
+        }
+        System.out.println("binary form of "+n+ " = "+ binnum);
+
+    }
+    public static void main(String[] args) {
+        dectobin(7);
+
+    }
 
 }
