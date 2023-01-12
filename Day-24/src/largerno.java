e305e1b7e8d10f6c8e8ac5e273a948bfe9379215
Bethegnt <tyagigagan30@gmail.com>
2023-01-12 16:06:05 +0530

    update

---
+++ Day-24/src/largerno.java
@@ -0,0 +1,12 @@
+public class largerno {
+    public static void main(String[] args) {
+        int n=7 , m=5;
+        if (n<=m) {
+            System.out.println("Greater number is :"+m);
+
+        }
+        else{
+            System.out.println("Greater number is :"+n);
+        }
+    }
+}
