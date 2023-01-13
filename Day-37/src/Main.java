d01f35243ad2779a0f699610771192abaaddf0c7
Bethegnt <tyagigagan30@gmail.com>
2023-01-13 14:59:25 +0530

    update

---
+++ Day-37/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("This is first case of code");
+        System.out.println("this is end case of code");
+    }
+
+    public static void system(String args[]) {
+        System.out.println("this is start for system");
+        System.out.println("this is end point for system");
+    }
+}
