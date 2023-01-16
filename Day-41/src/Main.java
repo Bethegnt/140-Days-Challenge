4c8fc5b2bca7e24fe272db5eec551b729d2e6660
Bethegnt <tyagigagan30@gmail.com>
2023-01-16 19:14:47 +0530

    update

--- Day-41/src/Main.java
+++ Day-41/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     static int c = 23;
     public static void main(String[] args) {
         int b = 13;
-       // Main obj = new Main(); non static variable is used by making obj.
+       // Main obj = new Main(); nonstatic variable is used by making obj.
        // System.out.println(obj.a);
         System.out.println(Main.a);
         System.out.println(b);
