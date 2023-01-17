fbef00aeed48946919043693b2edcdb470723b7c
Bethegnt <tyagigagan30@gmail.com>
2023-01-17 14:41:33 +0530

    update

--- Day-46/src/NameAddress.java
+++ Day-46/src/NameAddress.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class NameAddress {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Name: ");
-        String name = sc.next();
-        int age = sc.nextInt();
+        String name = sc.next();    //  print string name.
+        System.out.println("Enter Gender: ");
+        char gender = sc.next().charAt(0);  //  print character gender.
+        System.out.println("Enter Age: ");
+        int age = sc.nextInt(); //  print Age through number.
+        System.out.println("Enter Phone Number: ");
+        long phone = sc.nextLong(); //  print phone number.
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Gender: "+gender);
+        System.out.println("Phone No.: "+phone);
 
     }
 }
--- Day-47/src/Main.java
+++ Day-47/src/Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String fun;
+       do { Scanner sc = new Scanner(System.in);
+           System.out.println("Enter the first number: ");
+           int a = sc.nextInt();
+           System.out.println("Enter the second number: ");
+           int b = sc.nextInt();
+           int res;
+           System.out.println("operator: Add-A,Sub-S,Mul-M,Div-D: ");
+           char operator = sc.next().charAt(0);
+// Choose the operator
+           switch (operator){
+// Addition of the desired number:
+               case 'A':
+                   res = a+b;
+                   System.out.println("Add: "+res);
+                   break;
+// Subtraction of the desired number:
+               case 'S':
+                   res = a-b;
+                   System.out.println("Sub: "+res);
+                   break;
+// Multiply of the desired number:
+               case 'M':
+                   res = a*b;
+                   System.out.println("Mul: "+res);
+                   break;
+// Division of the desired number:
+               case 'D':
+                   res = a%b;
+                   System.out.println("Div: "+res);
+                   break;
+// default value if you cant press the right operator.
+               default:
+                   System.out.println("didn't choose any operator for further result.");
+           }
+           System.out.println("Do you want to continue(Press Yes or No) ");
+           fun =sc.next();
 
-        System.out.println("Hello world!");
+       }while(fun.equals("yes")||fun.equals("Yes"));
+       // equals function use for upper case or lower case anything press the ans should same.
+
+
     }
 }
