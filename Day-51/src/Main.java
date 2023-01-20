ba8a9ebedecb7ea716a2131b703ee17ce64f4264
Bethegnt <tyagigagan30@gmail.com>
2023-01-20 21:47:49 +0530

    update

---
+++ Day-51/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String og = "opengenus";
+        String og2 = new String(og);
+
+        System.out.println((og==og2) + " "+ (og.equals(og2)));
+        return ;
+
+     //  System.out.println("Hello world!");
+    }
+}
